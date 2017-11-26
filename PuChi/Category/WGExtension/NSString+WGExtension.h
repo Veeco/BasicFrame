ef58af3c662cf29fbe6934908d2245de39723f7f
Veeco <Jianwei>
2017-11-27 00:43:21 +0800

    优化分类

--- PuChi/Category/WGExtension/NSString+WGExtension.h
+++ PuChi/Category/WGExtension/NSString+WGExtension.h
@@ -12,9 +12,10 @@
 /**
  * 计算出文字总体高度
  * 参数 width 文字总体宽度
+ * 参数 font 文字字体
  * 返回 文字总体高度
  * 注意 传入的宽度与字体大小要大于0且宽度不小于字体大小
  */
-- (CGFloat)wg_calculateStringHeightWithWidth:(CGFloat)width fontSize:(CGFloat)fontSize;
+- (CGFloat)wg_calculateStringHeightWithWidth:(CGFloat)width font:(nonnull UIFont *)font;
 
 @end
--- PuChi/Category/WGExtension/NSString+WGExtension.m
+++ PuChi/Category/WGExtension/NSString+WGExtension.m
@@ -12,15 +12,16 @@
 /**
  * 计算出文字总体高度
  * 参数 width 文字总体宽度
+ * 参数 font 文字字体
  * 返回 文字总体高度
  * 注意 传入的宽度与字体大小要大于0且宽度不小于字体大小
  */
-- (CGFloat)wg_calculateStringHeightWithWidth:(CGFloat)width fontSize:(CGFloat)fontSize {
+- (CGFloat)wg_calculateStringHeightWithWidth:(CGFloat)width font:(nonnull UIFont *)font {
 
-    if (width <= 0.0 || fontSize <= 0.0 || width < fontSize) [NSException raise:@"计算文字高度出错" format:@"请检查传入的宽度与字体大小是否大于0且宽度不小于字体大小"];
+    if (width <= 0.0 || font.pointSize <= 0.0 || width < font.pointSize) [NSException raise:@"计算文字高度出错" format:@"请检查传入的宽度与字体大小是否大于0且宽度不小于字体大小"];
     
     // 设置属性
-    NSDictionary *dic = @{NSFontAttributeName : [UIFont systemFontOfSize:fontSize]};
+    NSDictionary *dic = @{NSFontAttributeName : font};
     
     // 返回高度
     return [self boundingRectWithSize:CGSizeMake(width, MAXFLOAT) options:NSStringDrawingUsesLineFragmentOrigin attributes:dic context:nil].size.height;
